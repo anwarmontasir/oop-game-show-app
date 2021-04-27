d73e0202fe49420f2a7bc4203c9c6df7f88bcbfd
anwarmontasir <anwarmontasir@gmail.com>
2021-04-27 15:04:56 -0700

    set up phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,17 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    addPhraseToDisplay() {
+        /* add letter placeholders to the display when the game starts */
+    }
+
+    checkLetter() {
+        /* check to see if the letter selected by the player matches a letter in the phrase */
+    }
+
+    showMatchedLetter() {
+        /* reveal the letter on the board that matches the player's selection */
+    }
+}
