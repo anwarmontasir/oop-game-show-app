6df757e68fe420d51f47fd9f2c2022f4756e9ec0
anwarmontasir <anwarmontasir@gmail.com>
2021-04-27 15:18:04 -0700

    create Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,31 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor(missed = 0, phrases, activePhrase = null) {
+        this.missed = missed;
+        this.phrases = phrases;
+        this.activePhrase = activePhrase;
+    }
+
+    startGame() {
+        /* hide start screen overlay, call getRandomPhrase, set activePhrase with chosen phrase, call addPhraseToDisplay */
+    }
+
+    getRandomPhrase() {
+        /* randomly retrieve phrase from phrases array and return it */
+    }
+
+    handleInteraction() {
+        /* checks to see if button clicked matches letter in phrase, then directs game based on correct or incorrect guess */
+    }
+
+    removeLife() {
+        /* remove a life from scoreboard */
+    }
+
+    checkForWin() {
+        /* has the player revealed all letters in active phrase */
+    }
+
+    gameOver() {
+        /* display start screen overlay, win or loss message */
+    }
+}
